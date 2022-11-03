1a4af4fdec0c5dcef933b9dd2aba4eb35d28be67
olivier <olivier@evaletolab.ch>
2022-11-03 17:01:12 +0100

    new: initial layout for desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,101 @@
 <template>
-  <div class="home container">
-
-    <!-- Digital identity -->
-    <div class="">
-        <h2>Protect your Digital Identity</h2>
-        <p>
-          Digital identity is a new reality, it appears complex or wird because we have no practice on that subject.
-          The goal of this application is to provide a few solutions to protect your identiy without the needs of a trusted thirdparties.
-        </p>
-        <input v-model="email" placeholder="enter email" type="email" required />
-        <input @click="submit" value="submit" type="submit"  v-bind:disabled="!validateEmail()" />
+  <div class="home">
+    <div class="left-container">
+      <div class="piggybank">
+        <img src="@/assets/piggybank.png"/>
+      </div>
+      <div class="">
+          <p>
+            Nous proposons au grand public l’accès aux deux 
+            principales monnaies numériques <b>sans intermédiaire</b>. 
+            <b>Seul vous</b>, et les personnes de votre choix y 
+            auront accès.
+          </p>
+      </div>
     </div>
+    <div class="right-container">
+      <div class="content">
+        <h1>piggybank.swiss</h1>
+        <p>L’accès aux monnaies numériques pour tous, avec 
+          des petits montants, de manière simple et sécurisée. 
+          Nous vous aidons à monter votre propre plan d’épargne 
+          numérique. 
+        </p>
+
+        <ul>
+          <li><a href="#">Une solution innovante</a></li>
+          <li><a href="#">Un service 100% Suisse</a></li>
+          <li><a href="#">Confiance & sécurité</a></li>
+          <li><a href="#">Calculer votre participation</a></li>
+        </ul>
+
+        <div class="action">
+
+            <input v-model="email" placeholder="enter email" type="email" required />
+            <button class="large" @click="submit"  v-bind:disabled="!validateEmail()" >JE SUIS INTERRESSE.E</button>
+        </div>
+      </div>
+    </div>    
   </div>
+
 </template>
 <style scoped lang="scss">
+  .home{
+    display: flex;
+    height: 100vh;
+    text-align: left;
+    .left-container {
+      width: 50vw;
+      max-width: 724px;
+      color: white;   
+      background: var(--color-primary);
 
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;      
+
+      padding: 40px;
+
+      .piggybank{
+        text-align: center;
+        margin: auto;
+        img{
+          width: 50%;
+        }
+      }
+    }
+    .right-container {
+      width: 50vw;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;      
+      padding: 40px;
+      h1{
+        font-weight: 600;
+        margin-bottom: 30px;
+      }
+
+      div.content{
+        max-width: 700px;
+
+        div.action{
+          width: 300px;
+        }
+      }
+
+    }
+    ul{
+      list-style: none;
+      margin-bottom: 50px;
+    }
+
+    button.large{
+      font-size: 1.4rem;
+      height: 4.5rem;
+      line-height: 4.5rem;
+      padding: 0 2rem;      
+    }
+  }
 </style>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
