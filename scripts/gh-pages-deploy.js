a4776cb06b1fce7e514c7dee0e2dfd73ac96c4c9
olivier <olivier@evaletolab.ch>
2022-11-04 13:52:52 +0100

    fix: exec call

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,10 +3,10 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    const branch = await execa('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+    const branch = (await execa('git', ['rev-parse', '--abbrev-ref', 'HEAD'])).stdout;
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
-    console.log("Building started...");
+    console.log("Building started from ",branch, "... ");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
